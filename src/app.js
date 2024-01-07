56e36cb6a4f1cdee52959a6ceb3c9db7840c722e
wraymond2017 <wraymond2017@outlook.com>
2024-01-07 00:21:32 -0500

    Almost Final Commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+  document.querySelector('#btn').addEventListener("click", () =>{
+  document.querySelector('#theExcuse').innerHTML = generateExcuse();
+  });
+  console.log('Hello Rigo from the console! ');
+};
+
+let generateExcuse = () => {
+  let pronoun =['A', 'The'];
+  let subject =['jogger','racoon', 'dog', 'driver', 'comedian', 'pinecone'];
+  let action =['took my', 'threw my', 'yelled at my', 'stole my', 'bit my'];
+  let possession =['homework', 'toe', 'car', 'shoe'];
+  let where =['on the street', 'in my house', 'in my driveway'];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);;
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+return pronoun[proIndx] + ' ' + subject[subIndex] + ' ' + action[actionIndex] + ' ' + possession[possessionIndex] + ' ' + where[whereIndex];
+};
